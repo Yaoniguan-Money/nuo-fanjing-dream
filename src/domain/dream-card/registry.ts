45322d119652005aa621243bceafb709407dbfb0
luy-0 <seven_ou@163.com>
2026-08-29 15:23:39 +0800

    生成很多图片

--- src/domain/dream-card/registry.ts
+++ src/domain/dream-card/registry.ts
@@ -1,5 +1,7 @@
 import jiuWeiTuDiShenCardJson from "../../../content/dream-cards/dream.jiu-wei-tu-di-shen.di-jiu-tan.json";
+import goubuPanguanCardJson from "../../../content/dream-cards/dream.goubu-panguan.he-ye-ji.json";
 import kailuJiangjunCardJson from "../../../content/dream-cards/dream.kailu-jiangjun.du-shan-ji.json";
+import saodiHeshangCardJson from "../../../content/dream-cards/dream.saodi-heshang.yu-huo-ji.json";
 import tangshiTaipoCardJson from "../../../content/dream-cards/dream.tangshi-taipo.zhi-hun-ji.json";
 import xianfengXiaojieCardJson from "../../../content/dream-cards/dream.xianfeng-xiaojie.qian-jie-qiao.json";
 import { dreamCardSchema, type DreamCard } from "./schema";
@@ -8,7 +10,9 @@ const registeredCards = [
   dreamCardSchema.parse(kailuJiangjunCardJson),
   dreamCardSchema.parse(xianfengXiaojieCardJson),
   dreamCardSchema.parse(jiuWeiTuDiShenCardJson),
-  dreamCardSchema.parse(tangshiTaipoCardJson)
+  dreamCardSchema.parse(tangshiTaipoCardJson),
+  dreamCardSchema.parse(saodiHeshangCardJson),
+  dreamCardSchema.parse(goubuPanguanCardJson)
 ] as const;
 const cardsById = new Map<string, DreamCard>(registeredCards.map((card) => [card.meta.id, card]));
 
